32e6ac46fa7e968bece56a1a2ae9c3814b485e12
marcelojlf23 <marcelo.jlf23@gmail.com>
2021-10-03 16:31:28 -0300

    estrutura de pastas inicial
    estrutura de pastas inicial

---
+++ .expo-shared/src/pages/Movies/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function Movies(){
+    return ( 
+        <View>
+            <Text>MEUS FILMES</Text>
+        </View>
+     );
+}
+ 
+export default Movies;
