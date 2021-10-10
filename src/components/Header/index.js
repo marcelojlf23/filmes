c357c6184b6eafc4427ceb32db284d20bdfdc905
marcelojlf23 <marcelo.jlf23@gmail.com>
2021-10-10 16:36:10 -0300

    using a hook to adding event to open Drawer when clicked in the button
    using a hook to adding event to open Drawer when clicked in the button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Container,MenuButton, Title } from './styles';
 
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 function Header() {
+
+    const navigation = useNavigation();
+
     return (
         <Container>
-            <MenuButton>
+            <MenuButton onPress={()=> navigation.openDrawer() }>
                 <Feather name="menu" size={36} color="#fff" />
                 <Title>React Prime</Title>
             </MenuButton>
