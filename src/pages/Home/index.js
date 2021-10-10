21532912ddf5e715c82eb2610b99d78982ab0598
marcelojlf23 <marcelo.jlf23@gmail.com>
2021-10-10 16:55:14 -0300

    creating and stylizing search input
    creating and stylizing search input

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-import { Container } from './styles';
+import { Container, SearchContainer, SearchButton, Input } from './styles';
+import { Feather } from '@expo/vector-icons';
 
 import Header from '../../components/Header';
 
@@ -9,6 +10,16 @@ function Home() {
     return (
         <Container>
             <Header title="React Prime"/>
+
+            <SearchContainer>
+                <Input
+                    placeholder="Ex Vingadores"
+                    placeholderTextColor="#ddd"
+                />
+                <SearchButton>
+                    <Feather name="search" size={30} color="#FFF"/>
+                </SearchButton>
+            </SearchContainer>
             <Text>HOME</Text>
         </Container>
     );
