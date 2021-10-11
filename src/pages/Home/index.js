51facc0467c8d2f3caf92857d4a0130409269c5c
marcelojlf23 <marcelo.jlf23@gmail.com>
2021-10-11 08:52:32 -0300

    adding property resize to image of the Banner
    adding property resize to image of the Banner

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -26,6 +26,7 @@ function Home() {
 
                 <BannerButton>
                     <Banner
+                        resizeMethod="resize"
                         source={{ uri: "https://images.unsplash.com/photo-1602461601079-fb03b7b35e61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80"}}
                     />
                 </BannerButton>
