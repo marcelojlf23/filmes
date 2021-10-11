f66ad04bca1279cc8817c0343c92cec0f3f0218d
marcelojlf23 <marcelo.jlf23@gmail.com>
2021-10-11 08:54:25 -0300

    changing opacity when clicked in the button and linking click event
    changing opacity when clicked in the button and linking click event

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -24,7 +24,8 @@ function Home() {
             <ScrollView>
                 <Title>Em Cartaz</Title>
 
-                <BannerButton>
+                <BannerButton 
+                    activeOpacity={0.9} onPress={()=>alert('teste')}>
                     <Banner
                         resizeMethod="resize"
                         source={{ uri: "https://images.unsplash.com/photo-1602461601079-fb03b7b35e61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80"}}
