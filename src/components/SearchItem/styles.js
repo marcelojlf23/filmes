57dce0c87a3635f094bcaaf0879fe80ccd8e351c
marcelojlf23 <marcelo.jlf23@gmail.com>
2021-10-23 17:08:06 -0300

    creating style to the serached movies
    creating style to the serached movies

--- src/components/SearchItem/styles.js
+++ src/components/SearchItem/styles.js
@@ -1,4 +1,12 @@
 
 import styled from "styled-components";
 
-export const Container = styled.TouchableOpacity``;
+export const Container = styled.TouchableOpacity`
+    padding: 14px;
+`;
+
+export const Banner = styled.Image`
+    width: 100%;
+    height: 140px;
+    border-radius: 8px;
+`;
